6f6c38de0d6e0f6ff127e5bc55f6363470f03dc2
PY.Lee <amstronglee@gmail.com>
2014-05-27 14:37:11 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,6 @@
-from bottle import route, run
 
 def application(environ, start_response):
      status = '200 OK'
      response_headers = [('Content-type','text/plain')]
      start_response(status, response_headers)
      return ['Hello World']
-
-
-@route('/hello')
-def hello():
-	return "Hello World! from bottle"
-
-run(host='0.0.0.0', port=8080, debug=True)
